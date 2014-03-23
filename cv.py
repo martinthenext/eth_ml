6ea8d994a32c4986b666982973c8e113896cd954
Martin Davtyan <martin.davtyan@gmail.com>
2014-03-23 19:42:31 +0100

    Naive Bayes on bag-of-words implemented, cv score is not that awful

--- cv.py
+++ cv.py
@@ -5,11 +5,27 @@ import sys
 import data
 import models
 
+import numpy as np
+
+from sklearn import cross_validation
+
 annotations = data.load_data(sys.argv[1])
 
-def get_error_rate(classifier_class, annotations):
+def get_error_rate(classifier_class, annotations, nfolds=10):
+  sumcostAvg=0
+  folds = cross_validation.KFold(len(annotations),n_folds=nfolds)
   classifier = classifier_class()
-  classifier.train(annotations[:-1])
-  return int(classifier.predict([annotations[-1]]) == annotations[-1].get_group_number())
+
+  for train_indices, test_indices in folds:
+    trainset=np.array([annotations[i] for i in train_indices[:]])
+    classifier.train(trainset)
+    testset=np.array([annotations[i] for i in test_indices[:]])
+    predictions=classifier.predict(testset)
+    sumcost=0
+    for i in range(len(predictions)):
+      sumcost+=int(predictions[i] != annotations[test_indices[i]].get_group_number())
+    sumcostAvg+=float(sumcost)/float(len(testset))
+
+  return float(sumcostAvg)/float(nfolds)
 
 print get_error_rate(models.VeryVeryNaiveBayes, annotations)
