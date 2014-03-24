acfa10fc4633636ca5538cd272f4bb3464587f3b
Martin Davtyan <martin.davtyan@gmail.com>
2014-03-24 16:15:39 +0100

    Hand-annotated results show that MTurk is mostly right in conflicts with the classifier

--- mturk_classifier_agreement.py
+++ mturk_classifier_agreement.py
@@ -13,6 +13,9 @@ from data import load_data, Annotation
 from models import VeryVeryNaiveBayes
 import numpy as np
 
+sys.stdout = codecs.getwriter('utf-8')(sys.__stdout__)
+
+
 # train a classifier on non-ambiguous annotations
 
 nonambig_annotations = load_data(sys.argv[1])
@@ -30,4 +33,10 @@ with codecs.open(sys.argv[2], 'r', 'utf-8') as f:
 predicted_group_numbers = classifier.predict(mturk_annotations)
 voted_group_numbers = [annotation.get_group_number() for annotation in mturk_annotations]
 agreement = [int(predicted == voted) for predicted, voted in zip(predicted_group_numbers, voted_group_numbers)]
-print np.mean(agreement)
+
+sys.stdout.write('sep=|\n')
+sys.stdout.write(str(np.mean(agreement)) + '\n')
+sys.stdout.write('MTURK ANSWER|TASK|CLASSIFIER ANSWER\n')
+
+for mturk_annotation, predicted in zip(mturk_annotations, predicted_group_numbers):
+  sys.stdout.write('%s|%s|%s\n' % (mturk_annotation.grp, mturk_annotation.get_highlighted_repr(), Annotation.GROUP_NAMES[predicted]) )
