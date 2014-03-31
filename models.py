04b25c583006cbcccdc7cfe4716e2a66c9cdeee1
Martin Davtyan <martin.davtyan@gmail.com>
2014-03-31 09:24:06 +0200

    Proper infrastructure for vectorizer introduced

--- models.py
+++ models.py
@@ -1,22 +1,42 @@
-''' Here classifiers live
-    They train on Annotation objects and predict group codes
+''' Here classifiers live.
+
+    Classifiers and Vectorizers here are wrappers around according
+    sklearn objects that classify and vectorize annotations
 '''
 
 import numpy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-class VeryVeryNaiveBayes(object):
+''' Wrapper aroung sklearn to vectorize Annotation objects
+'''
+class FullContextVectorizer(object):
   def __init__(self):
-    self.classifier = MultinomialNB()
     self.vectorizer = CountVectorizer()
 
+  def fit_transform(self, annotations):
+    return self.vectorizer.fit_transform(
+      [annotation.get_context_string() for annotation in annotations])
+
+  def transform(self, annotations):
+    return self.vectorizer.transform(
+      [annotation.get_context_string() for annotation in annotations])
+
+''' Annotation classifier base class. Subclass it and specify
+    a vectorizer and a classifiers
+'''
+class AnnotationClassifier(object):
   def train(self, annotations):
-    X = self.vectorizer.fit_transform([annotation.get_context_string() for annotation in annotations])
+    X = self.vectorizer.fit_transform(annotations)
     y = numpy.array([annotation.get_group_number() for annotation in annotations])
 
     self.classifier.fit(X, y)
 
   def predict(self, annotations):
-    X = self.vectorizer.transform([annotation.get_context_string() for annotation in annotations])
+    X = self.vectorizer.transform(annotations)
     return self.classifier.predict(X)
+
+class VeryVeryNaiveBayes(AnnotationClassifier):
+  def __init__(self):
+    self.classifier = MultinomialNB()
+    self.vectorizer = FullContextVectorizer()
