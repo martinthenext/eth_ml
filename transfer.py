43e3d382c73b45da71f6b92a0d48da8852506214
Martin Davtyan <martin.davtyan@gmail.com>
2014-04-29 01:06:27 +0200

    First transfer classifier, needs testing

--- transfer.py
+++ transfer.py
@@ -1,13 +1,55 @@
-from data import FullContextBagOfWordsLeftRightCutoff
+from data import Annotation, FullContextBagOfWordsLeftRightCutoff
+import itertools
+import random
 
 '''
+
+train_source - train a MultinomialNB (with weight 1)
+train_target_online - partial_fit on it with weight `target_weight` > 1
+
+'''
+class WeightedPartialFitPassiveTransferClassifier(object):
+  def __init__(self, target_weight):
+    self.classifier = MultinomialNB()
+    self.target_weight = target_weight
+    self.vectorizer = FullContextBagOfWordsLeftRightCutoff(9)
+
+  # Train on unambiguous annotatios which have a group number
+  def train_source(self, annotations):
+    X = self.vectorizer.fit_transform(annotations)
+    y = numpy.array([annotation.get_group_number() for annotation in annotations])
+
+    self.classifier.fit(X, y)
+
+  # Train on ambiguous annotations with according group number labels
+  def train_target_online(self, annotations, labels):
+    X = self.vectorizer.transform(annotations)
+    y = numpy.array(labels)
+
+    weight_vector = [self.target_weight] * len(annotations)
+    self.classifier.partial_fit(X, y, Annotation.GROUP_MAPPING.values(), weight_vector)
+
+  def predict(self, annotations):
+    X = self.vectorizer.transform(annotations)
+    probs = self.classifier.predict_proba(X) # [n_samples, n_classes]
+    return numpy.array([self.get_group_number(annotation, row)
+     for row, annotation in itertools.izip(probs, annotations)])
+
+  # Passive learner
+  def pick_and_train(annotation_label_tuple_pool, sample_size):
+    annotation_label_tuples = random.sample(annotation_label_tuple_pool, sample_size)
+    annotations, labels = zip(*annotation_label_tuples)
+    self.train_target_online(annotations, labels)
+
+'''
+TODO:
 
 # Training
 
 This classifier has two classifiers inside:
 
 1. Source classifier, trained on unambiguous annotations
-2. Targer classifier, partially fitted on ambigous annotations from the pool
+2. Target classifier, partially fitted on ambigous annotations from the pool
 
 # Prediction
 
@@ -21,17 +63,3 @@ Target classifier should have the same vectorizer as source one, otherwize it
 would be impossible to form a proper dictionary
 
 '''
-
-class InductiveTransferPassiveClassifier(object):
-  def __init__(self):
-    self.source_classifier = MultinomialNB()
-    self.target_classifier = MultinomialNB()
-    self.vectorizer = FullContextBagOfWordsLeftRightCutoff(9)
-
-  # Train on unambiguous annotatios which have a group number
-  def train_source(self, annotations):
-    pass
-
-  # Train on ambiguous annotations with according group number labels
-  def train_target_online(self, annotations, labels):
-    pass
