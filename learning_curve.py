bc4ef43db07128c7e1259648ad5552133629849d
Martin Davtyan <martin.davtyan@gmail.com>
2014-05-02 00:59:40 +0200

    Basic learning curve measurement implemented

--- learning_curve.py
+++ learning_curve.py
@@ -16,28 +16,55 @@ from data import load_ambiguous_annotations_labeled
 from sklearn.cross_validation import train_test_split
 import numpy as np
 import sys
+import itertools
+from copy import deepcopy
 # TODO make the deprecation warning go away
 from sklearn.externals import joblib
 
-N_SIMULATIONS = 1000
-agreement_before = np.zeros(N_SIMULATIONS)
-agreement_after = np.zeros(N_SIMULATIONS)
+classifier_pickle_filename = sys.argv[1]
+annotations_labeled_filename = sys.argv[2]
 
-annotations, labels = load_ambiguous_annotations_labeled(sys.argv[2])
+def get_classifier_agreement_increase_table(target_weight_list, n_simulations = 1000):
+  agreement_before = np.zeros(n_simulations)
+  agreement_after = np.zeros(n_simulations)
+  annotations, labels = load_ambiguous_annotations_labeled(annotations_labeled_filename)
+  result = ""
 
-for weight in [10]:
-  for i in xrange(N_SIMULATIONS):
-    classifier = joblib.load(sys.argv[1])
+  for weight in target_weight_list:
+    for i in xrange(n_simulations):
+      classifier = joblib.load(classifier_pickle_filename)
 
-    pool_annotations, test_annotations, pool_labels, test_labels = train_test_split(
-      annotations, labels, test_size = 0.33)  
+      pool_annotations, test_annotations, pool_labels, test_labels = train_test_split(
+        annotations, labels, test_size = 0.33)  
 
-    # validate the initial state of the classifier
-    agreement_before[i] = get_agreement(classifier, (test_annotations, test_labels))
+      # validate the initial state of the classifier
+      agreement_before[i] = get_agreement(classifier, (test_annotations, test_labels))
 
-    # test: target train on the entire pool, validate again 
-    classifier.target_weight = weight
-    classifier.train_target_online(pool_annotations, pool_labels)
-    agreement_after[i] = get_agreement(classifier, (test_annotations, test_labels))
+      # test: target train on the entire pool, validate again 
+      classifier.target_weight = weight
+      classifier.train_target_online(pool_annotations, pool_labels)
+      agreement_after[i] = get_agreement(classifier, (test_annotations, test_labels))
 
-  print str(weight), np.mean(agreement_after - agreement_before)
+    result += str(weight), np.mean(agreement_after - agreement_before)
+  return result
+
+# this does not change the state of classifier
+def get_accuracy_progression(classifier_to_measure, annotations, labels, target_weight):
+  classifier = deepcopy(classifier_to_measure)
+  classifier.target_weight = target_weight
+
+  pool_annotations, test_annotations, pool_labels, test_labels = train_test_split(
+        annotations, labels, test_size = 0.33) 
+
+  # initialize the accuracy list with the initial accuracy
+  accuracy_list = [ get_agreement(classifier, (test_annotations, test_labels)) ]
+  for pool_annotation, pool_label in itertools.izip(pool_annotations, pool_labels):
+    classifier.train_target_online([pool_annotation], [pool_label])
+    accuracy_list.append( get_agreement(classifier, (test_annotations, test_labels)) )
+
+  return accuracy_list
+
+classifier = joblib.load(classifier_pickle_filename)
+annotations, labels = load_ambiguous_annotations_labeled(annotations_labeled_filename)
+
+print get_accuracy_progression(classifier, annotations, labels, 1000)
