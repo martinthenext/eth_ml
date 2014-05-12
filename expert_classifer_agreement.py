39b586abf140f19acadc0ddbcd20ea2679e4c338
hanveiga <han.veiga@gmail.com>
2014-05-12 22:42:14 +0100

    fixed a bug

--- expert_classifer_agreement.py
+++ expert_classifer_agreement.py
@@ -1,11 +1,10 @@
 import sys
 import codecs
 import models
-import data
 import numpy as np
 from sklearn.externals import joblib
 import argparse
-import data as test
+import data
 
 def get_expert_agreement(classifier, mturk_labeled_data):
   # read mturk annotations
@@ -15,7 +14,7 @@ def get_expert_agreement(classifier, mturk_labeled_data):
   voted_group_numbers, indices = zip(*[ (data.Annotation.GROUP_MAPPING[labels[index]], index) for index in range(len(labels)) 
                           if labels[index] in data.Annotation.GROUP_NAMES ])
 
-  voted_group_numbers_strict = [0] * len(predicted_group_numbers)
+  voted_group_numbers_strict = [-1] * len(predicted_group_numbers)
   voted_group_numbers_strict = np.array(voted_group_numbers_strict)
   voted_group_numbers_strict[list(indices)] = voted_group_numbers
   restricted_group_numbers = predicted_group_numbers[list(indices)]
@@ -26,7 +25,7 @@ def get_expert_agreement(classifier, mturk_labeled_data):
 
 def get_mturk_pickled_classifier_agreement(classifier_pickle_file, mturk_vote_file_path, **kwargs):
   classifier = joblib.load(classifier_pickle_file)
-  mturk_labeled_data = test.load_ambiguous_annotations_labeled_generic(mturk_vote_file_path)
+  mturk_labeled_data = data.load_ambiguous_annotations_labeled_generic(mturk_vote_file_path)
   return get_expert_agreement(classifier, mturk_labeled_data)
 
 a,b = get_mturk_pickled_classifier_agreement(sys.argv[1],sys.argv[2])
