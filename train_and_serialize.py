425cee87b641cb9315b9c35c61b2770c160b86b8
Martin Davtyan <martin.davtyan@gmail.com>
2014-05-15 23:34:16 +0200

    Using joblib.Parallrallel to use multiple cores

--- train_and_serialize.py
+++ train_and_serialize.py
@@ -5,7 +5,6 @@
 This will train a transfer classifier on unambiguous data and serialize it to file with sklearn.joblib
 
 ARGUMENT 1: SSC xml file to extract non-ambiguous annotations from
-ARGUMENT 2: file name to serialize the classifier to
 
 Load like: joblib.load('pickles.nobackup/EMEA_OptionAwareNaiveBayesLeftRight')
 
@@ -22,6 +21,7 @@ def train_and_serialize(ssc_file_path, serialization_path, classifier_class, tra
   if dataset_fraction:
     step = int(1/dataset_fraction)
     train_set = itertools.islice(unambig_annotations, 0, None, step)
+    serialization_path += '_fraction' + str(dataset_fraction)
   else:
     train_set = unambig_annotations
   classifier = classifier_class(**kwargs)
@@ -32,5 +32,9 @@ def train_and_serialize(ssc_file_path, serialization_path, classifier_class, tra
 
   joblib.dump(classifier, serialization_path)
 
-train_and_serialize(sys.argv[1], 'pickles.nobackup/WeightedPartialFitPassiveTransferClassifier_Medline_weight1000_fraction025', 
-  transfer.WeightedPartialFitPassiveTransferClassifier, dataset_fraction=0.25, target_weight=1000)
+def run(frac):
+  train_and_serialize(sys.argv[1], 'pickles.nobackup/WeightedPartialFitPassiveTransferClassifier2_Medline', 
+    transfer.WeightedPartialFitPassiveTransferClassifier2, dataset_fraction=frac)
+  return None
+
+joblib.Parallel(n_jobs=4)(joblib.delayed(run)(frac) for frac in [None, 0.1, 0.05, 0.01])
