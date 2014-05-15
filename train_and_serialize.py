d12bf4e880343ae99369610222ea2f3fb4be700b
Martin Davtyan <martin.davtyan@gmail.com>
2014-05-15 15:04:20 +0200

    New results double-checked with regards to #14 - the same script compare_vectorizers produces different results on old and new datasets

--- train_and_serialize.py
+++ train_and_serialize.py
@@ -15,9 +15,15 @@ import transfer
 import data
 import sys
 from sklearn.externals import joblib
+import itertools
 
-def train_and_serialize(ssc_file_path, serialization_path, classifier_class, transfer=True, **kwargs):
+def train_and_serialize(ssc_file_path, serialization_path, classifier_class, transfer=True, dataset_fraction=None, **kwargs):
   unambig_annotations = data.load_unambiguous_annotations(ssc_file_path)
+  if dataset_fraction:
+    step = int(1/dataset_fraction)
+    train_set = itertools.islice(unambig_annotations, 0, None, step)
+  else:
+    train_set = unambig_annotations
   classifier = classifier_class(**kwargs)
   if transfer:
     classifier.train_source(unambig_annotations)
@@ -25,3 +31,6 @@ def train_and_serialize(ssc_file_path, serialization_path, classifier_class, tra
     classifier.train(unambig_annotations)
 
   joblib.dump(classifier, serialization_path)
+
+train_and_serialize(sys.argv[1], 'pickles.nobackup/WeightedPartialFitPassiveTransferClassifier_Medline_weight1000_fraction025', 
+  transfer.WeightedPartialFitPassiveTransferClassifier, dataset_fraction=0.25, target_weight=1000)
