b842f6a7852d1a9c06c2fa6e4f016710e412acc0
hanveiga <han.veiga@gmail.com>
2014-05-27 22:31:25 +0100

    Implemented transfer learning by combining probability estimation of classifiers trained on source and target respectively

--- transfer.py
+++ transfer.py
@@ -100,3 +100,56 @@ class WeightedSVMHuberPartialFitPassiveTransferClassifier(WeightedPartialFitPass
     self.classifier = SGDClassifier(loss='modified_huber')
     self.target_weight = target_weight
     self.vectorizer = FullContextBagOfWordsLeftRightCutoff(9)
+
+class CombinedProbTransferClassifier(object):
+  def __init__(self, target_weight=0.5):
+    self.source_classifier = MultinomialNB()
+    self.target_classifier = MultinomialNB()
+    self.beta = target_weight
+    self.vectorizer = FullContextBagOfWordsLeftRightCutoff(9)
+
+  # Train on unambiguous annotatios which have a group number
+  def train_source(self, annotations):
+    X = self.vectorizer.fit_transform(annotations)
+    y = numpy.array([annotation.get_group_number() for annotation in annotations])
+
+    self.source_classifier.fit(X, y)
+
+  # Train on ambiguous annotations with according group labels
+  def train_target_online(self, annotations, labels):
+    X = self.vectorizer.transform(annotations)
+    y = numpy.array([Annotation.GROUP_MAPPING[label] for label in labels])
+
+    self.target_classifier.partial_fit(X, y, Annotation.GROUP_MAPPING.values())
+
+  def predict(self, annotations):
+    X = self.vectorizer.transform(annotations)
+    source_prob = self.source_classifier.predict_proba(X) # [n_samples, n_classes]
+    target_prob = self.target_classifier.predict_proba(X)
+    combined_prob = self.beta*source_prob + (1-self.beta)*target_prob
+
+    return numpy.array([self.get_group_number(annotation, row)
+     for row, annotation in itertools.izip(combined_prob, annotations)])
+
+  def get_group_number_prob_pair(self, annotation, prob_vector):
+    group_option_indices = annotation.get_group_number()
+    group_option_prob = [prob_vector[group_option_index] for group_option_index in group_option_indices]
+    return max(zip(group_option_indices, group_option_prob), key = lambda (index, prob): prob)
+ 
+  def get_group_number(self, annotation, prob_vector):
+    group_index, _ = self.get_group_number_prob_pair(annotation, prob_vector)
+    return group_index
+
+  # tested, results for the classifier trained on source are not random
+  def get_max_probability(self, annotation, prob_vector):
+    _, prob = self.get_group_number_prob_pair(annotation, prob_vector)
+    return prob
+
+  def get_prob_estimates(self, annotations):
+    X = self.vectorizer.transform(annotations)
+    source_prob = self.source_classifier.predict_proba(X) # [n_samples, n_classes]
+    target_prob = self.target_classifier.predict_proba(X)
+    combined_prob = self.beta*source_prob + (1-self.beta)*target_prob
+
+    return numpy.array([self.get_max_probability(annotation, row)
+      for row, annotation in itertools.izip(combined_prob, annotations)])
